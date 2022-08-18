d6fe8d5b71523c9f192b4a140f516078a63d8426
MarioFBSantos <mariosantos3197@gmail.com>
2022-08-18 20:44:48 -0300

    gh pages

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -13,61 +13,12 @@
     <q-page-container>
       <router-view />
     </q-page-container>
-     <!-- <q-footer elevated>
-        <q-toolbar>
-          <q-toolbar-title>Pezinho pog</q-toolbar-title>
-        </q-toolbar>
-      </q-footer> -->
   </q-layout>
 </template>
 
 <script lang="ts">
 import EssentialLink from 'components/EssentialLink.vue'
 
-const linksData = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-];
 
 import { Vue, Component } from 'vue-property-decorator';
 
@@ -76,7 +27,6 @@ import { Vue, Component } from 'vue-property-decorator';
 })
 export default class MainLayout extends Vue {
   leftDrawerOpen = false;
-  essentialLinks = linksData;
 }
 </script>
 
